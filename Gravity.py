5dbdc590c873d4dbef0115b2b716afc7f6f60ddb
Ahren Chen <58377458+Chennywise@users.noreply.github.com>
2022-08-30 19:38:50 -0400

    Update Gravity.py

--- Gravity.py
+++ Gravity.py
@@ -4,8 +4,6 @@ import math
 def distanceTwoPoints(point1,point2):
     return (((point1[0]-point2[0])**2)+((point1[1]-point2[1])**2)+((point1[2]-point2[2])**2))**(1/2)
 
-
-
 gravity = 0.0004
 
 #self.coords = coords
